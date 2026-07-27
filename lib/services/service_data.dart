7a0e56879071c62b71b71800a03f43a20a6d5f13
raihanauto <rahim.ahmad001100@gmail.com>
2026-07-27 14:00:43 +0600

    Create service_data.dart

---
+++ lib/services/service_data.dart
@@ -0,0 +1,20 @@
+import '../models/service_model.dart';
+
+final List<ServiceModel> serviceList = [
+  ServiceModel(
+    title: "Engine Oil Change",
+    description: "Premium engine oil replacement.",
+  ),
+  ServiceModel(
+    title: "Engine Repair",
+    description: "Complete engine servicing.",
+  ),
+  ServiceModel(
+    title: "Brake Service",
+    description: "Brake inspection and repair.",
+  ),
+  ServiceModel(
+    title: "Bike Wash",
+    description: "Professional bike cleaning.",
+  ),
+];
