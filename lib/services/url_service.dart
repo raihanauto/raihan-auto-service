0512be1be3a901f73bce6f078c36e4e3fcc82744
raihanauto <rahim.ahmad001100@gmail.com>
2026-07-27 23:39:31 +0600

    Update url_service.dart

--- lib/services/url_service.dart
+++ lib/services/url_service.dart
@@ -11,4 +11,23 @@ class UrlService {
       );
     }
   }
+
+  static Future<void> call(String number) async {
+    final uri = Uri.parse("tel:$number");
+
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    }
+  }
+
+  static Future<void> whatsapp(String number) async {
+    final uri = Uri.parse("https://wa.me/88$number");
+
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(
+        uri,
+        mode: LaunchMode.externalApplication,
+      );
+    }
+  }
 }
