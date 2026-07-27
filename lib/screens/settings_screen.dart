91c292f786da6baa783fe611a9323fde3739300a
raihanauto <rahim.ahmad001100@gmail.com>
2026-07-27 14:05:36 +0600

    Create settings_screen.dart

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: const Center(
+        child: Text(
+          "Settings Coming Soon",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
