547594fe995fd5163d8501f89abd59e7892f563c
raihanauto <rahim.ahmad001100@gmail.com>
2026-07-27 23:36:06 +0600

    Update contact_info.dart

--- lib/services/contact_info.dart
+++ lib/services/contact_info.dart
@@ -1,6 +1,18 @@
 class ContactInfo {
-  static const phone = "+8801XXXXXXXXX";
-  static const whatsapp = "+8801XXXXXXXXX";
+  static const phone1 = "01739781138";
+  static const phone2 = "01711969160";
 
-  static const address = "Raihan Auto Service";
+  static const whatsapp = "01739781138";
+
+  static const address =
+      "লালন সড়ক, মহিলা কলেজ সংলগ্ন, হরিনাকুন্ডু, ঝিনাইদহ";
+
+  static const facebook =
+      "https://www.facebook.com/raihanauto38";
+
+  static const youtube =
+      "https://youtube.com/@raihanauto?si=lsR8Efg6S2s0E28u";
+
+  static const googleMap =
+      "https://maps.app.goo.gl/jUvFQhqCqkDrTQwa9";
 }
