e2b965facaaa36ad66d0bee027d0124a160697bb
raihanauto <rahim.ahmad001100@gmail.com>
2026-07-27 13:50:05 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const RaihanAutoServiceApp());
-}
-
-class RaihanAutoServiceApp extends StatelessWidget {
-  const RaihanAutoServiceApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Raihan Auto Service',
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Raihan Auto Service'),
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to Raihan Auto Service',
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(const RaihanAutoApp());
 }
