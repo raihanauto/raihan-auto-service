c258ab113533a7fb826358f09c7568ad43bad55d
raihanauto <rahim.ahmad001100@gmail.com>
2026-07-27 14:14:33 +0600

    Create app_links.dart

---
+++ lib/services/app_links.dart
@@ -0,0 +1,10 @@
+class AppLinks {
+  static const facebook =
+      "https://www.facebook.com/raihanauto38";
+
+  static const youtube =
+      "https://youtube.com/@raihanauto?si=lsR8Efg6S2s0E28u";
+
+  static const maps =
+      "https://maps.app.goo.gl/jUvFQhqCqkDrTQwa9";
+}
