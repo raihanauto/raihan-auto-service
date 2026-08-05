d281c548905c31795051843810b774d60af2facb
raihanauto <rahim.ahmad001100@gmail.com>
2026-08-05 13:40:47 +0600

    Update service_model.dart

--- lib/models/service_model.dart
+++ lib/models/service_model.dart
@@ -1,9 +1,27 @@
+import 'package:flutter/material.dart';
+
 class ServiceModel {
   final String title;
+  final String englishTitle;
+  final IconData icon;
   final String description;
+  final String duration;
+  final List<String> works;
+  final String recommendedInterval;
+  final List<String> symptoms;
+  final String tip;
+  final List<Map<String, String>> faq;
 
-  ServiceModel({
+  const ServiceModel({
     required this.title,
+    required this.englishTitle,
+    required this.icon,
     required this.description,
+    required this.duration,
+    required this.works,
+    required this.recommendedInterval,
+    required this.symptoms,
+    required this.tip,
+    required this.faq,
   });
 }
