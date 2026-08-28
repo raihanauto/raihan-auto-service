362618e76f7b8c62fde93969cc2135f3d5d11fc6
raihanauto <rahim.ahmad001100@gmail.com>
2026-08-28 18:51:25 +0600

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:raihan_auto_service/main.dart';
 
-void main() {
-  testWidgets('Raihan Auto Service app loads', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-
-    expect(find.byType(MyApp), findsOneWidget);
-  });
-}
